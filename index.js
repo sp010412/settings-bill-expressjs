f7dc03fe94c5d4d0891c1417f41e43f6d6ecb2e0
sp010412 <smajwede7@gmail.com>
2021-08-06 06:56:36 +0200

    App functionalities completed

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require('./settings-bill');
+const handlebarSetup = exphbs({
+    partialsDir: "./views/partials",
+    viewPath: './views',
+    layoutsDir: './views/layouts'
+});
 
 const app = express();
+const settingsBill = SettingsBill();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+
+app.engine('handlebars', handlebarSetup);
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //default route  
-app.get('/', function(req, res){
-    res.render('index');
+app.get('/', function (req, res) {
+    res.render('index', {
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.totals()
+    });
 });
 
-app.post('/settings ', function(req, res){
- 
+app.post('/settings', function (req, res) {
+
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    });
+
+
+    res.redirect('/');
 });
 
-app.get('/action ', function(req, res){
-
+app.post('/action', function (req, res) {
+    settingsBill.recordAction(req.body.actionType)
+    res.redirect('/');
 });
 
-app.get('/action/:type ', function(req, res){
-
+app.get('/actions', function (req, res) {
+    res.render('actions', { actions: settingsBill.actions() });
 });
 
-app.post('/actions ', function(req, res){
 
+app.get('/actions/:actionType', function (req, res) {
+    const actionType = req.params.actionType;
+    res.render('actions', { actions: settingsBill.actionsFor(actionType) });
 });
 
-const PORT = process.env.PORT || 3011;
+const PORT = process.env.PORT || 3012;
 
-app.listen(PORT, function (){
+app.listen(PORT, function () {
     console.log("App started at port:", PORT)
 });
